3dff8a0061b2ba41446a98954678c419cbda00f4
Denys <deniska2002kulikov@gmail.com>
2026-05-05 16:10:43 +0300

    GIA-82 add coverage

--- src/test/java/org/gym/crm/service/iml/TraineeServiceImplTest.java
+++ src/test/java/org/gym/crm/service/iml/TraineeServiceImplTest.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
