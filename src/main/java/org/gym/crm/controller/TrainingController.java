e954b17d8e52db7a59382d855ca289e75e8c90d8
Denys Kulikov <115070319+borderForNoone@users.noreply.github.com>
2026-05-22 10:37:31 +0300

    GIA-148 Add Swagger annotations to endpoints
    * GIA-78 remove public for mapper test
    
    * GIA-148 Add Swagger annotations to endpoints
    
    * GIA-148 fix comment

--- src/main/java/org/gym/crm/controller/TrainingController.java
+++ src/main/java/org/gym/crm/controller/TrainingController.java
@@ -1,10 +1,17 @@
 package org.gym.crm.controller;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.gym.crm.dto.TrainingRequestDTO;
 import org.gym.crm.facade.GymFacade;
+import org.gym.crm.rest.ErrorResponse;
 import org.gym.crm.rest.TrainingTypeResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,20 +22,63 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Training", description = "Operations related to trainings")
 @RestController
-@RequestMapping("${app.api.base-path}")
+@RequestMapping("${app.api.base-path}/trainings")
 @RequiredArgsConstructor
 public class TrainingController {
     private final GymFacade facade;
 
-    @PostMapping("/trainings")
+    @Operation(summary = "Add new training", description = "Creates a new training session with the specified trainee, trainer, date, and duration")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Training created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )),
+            @ApiResponse(responseCode = "401", description = "Unauthorized access",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    ))
+    })
+    @PostMapping
     public ResponseEntity<Void> addTraining(@RequestBody @Valid TrainingRequestDTO request) {
         facade.createTraining(request);
 
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/trainings/types")
+    @Operation(summary = "Get training types", description = "Returns list of all available training types")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of training types",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = TrainingTypeResponse.class))
+                    )),
+            @ApiResponse(responseCode = "401", description = "Unauthorized access",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )),
+            @ApiResponse(responseCode = "404", description = "Training types not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    ))
+    })
+    @GetMapping("/types")
     public ResponseEntity<List<TrainingTypeResponse>> getTrainingTypes() {
         return ResponseEntity.ok(facade.getTrainingTypes());
     }
--- src/test/java/org/gym/crm/config/TestDatabaseConfig.java
+++ src/test/java/org/gym/crm/config/TestDatabaseConfig.java
@@ -1,15 +1,35 @@
 package org.gym.crm.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource(value = "classpath:application-test.yml", factory = YamlPropertySourceFactory.class)
 public class TestDatabaseConfig {
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.driver}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+
+        return dataSource;
+    }
+
     @Bean
     public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
         DataSourceInitializer initializer = new DataSourceInitializer();
