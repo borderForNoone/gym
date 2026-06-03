823df2c0bb69688738c7190473a5e86a3bf0d6c8
Denys Kulikov <115070319+borderForNoone@users.noreply.github.com>
2026-06-03 14:14:23 +0300

    GIA-217 Configure Spring Security base setup
    * GIA-181 Fix readme
    
    * GIA-217 Configure Spring Security base setup
    
    * GIA-217 Fix sonar exclusions

--- src/test/java/com/gym/crm/controller/AuthControllerTest.java
+++ src/test/java/com/gym/crm/controller/AuthControllerTest.java
@@ -12,6 +12,7 @@ import org.gym.crm.rest.LoginChangeRequest;
 import org.gym.crm.rest.LoginRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @Import(ApiExceptionHandler.class)
 class AuthControllerTest {
     private static final String USERNAME = "Simone.Radcliffe";
